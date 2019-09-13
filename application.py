d66974146d421f4f1ca98fa9dfb67def17cae0b2
George Rady <georgemagdy98@gmail.com>
2019-09-13 13:47:13 +0200

    Got things work

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -8,8 +8,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+# if not os.getenv("postgres://sqrnjpvrexcfat:35430cad4ba40571270335938b1737575f6b1469a1fb8bca3c9aa2736de39114@ec2-23-21-148-223.compute-1.amazonaws.com:5432/d6qcdnha05od0a"):
+#     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -17,10 +17,10 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgres://sqrnjpvrexcfat:35430cad4ba40571270335938b1737575f6b1469a1fb8bca3c9aa2736de39114@ec2-23-21-148-223.compute-1.amazonaws.com:5432/d6qcdnha05od0a")
 db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
